b36525bc70a322eb0e150b9b765b0f8a280fca31
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-01-29 18:23:36 -0500

    create a function to run after all html has loaded o the page

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,13 +3,13 @@
 // in the html.
 
 
-
-$(function () {
+//add ready for document to load AFTER HTML
+$(document).ready(function() {
   // TODO: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
   $('.saveBtn').click(function() {
     alert('working')
   });
-  // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
@@ -26,6 +26,8 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
 });
+
+
 // TODO: Add code to display the current date in the header of the page.
 // create variable for date using m=Moment API
 var todayDate = moment().format('dddd, MMM Do');
