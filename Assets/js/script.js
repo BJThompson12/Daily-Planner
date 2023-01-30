ae4633c722c342b8bbd9cc8e4a920775948df4d4
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-01-30 14:55:06 -0500

    format documents

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -50,26 +50,24 @@ $(document).ready(function () {
       }
     });
   }
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+
   //create GET to select the local storage inputs and place on the page
   // select the id of the time block and then the id of it
   // get the value from the local storage that has been set
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour14 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
+  $('#hour9 .description').val(localStorage.getItem('hour9'));
+  $('#hour10 .description').val(localStorage.getItem('hour10'));
+  $('#hour11 .description').val(localStorage.getItem('hour11'));
+  $('#hour12 .description').val(localStorage.getItem('hour12'));
+  $('#hour13 .description').val(localStorage.getItem('hour13'));
+  $('#hour14 .description').val(localStorage.getItem('hour14'));
+  $('#hour14 .description').val(localStorage.getItem('hour15'));
+  $('#hour16 .description').val(localStorage.getItem('hour16'));
+  $('#hour17 .description').val(localStorage.getItem('hour17'));
+
   // call the function
   getTime();
 });
 
-// TODO: Add code to display the current date in the header of the page.
 // create variable for date using Moment API
 var todayDate = moment().format('dddd, MMM Do');
 $('#currentDay').html(todayDate);
