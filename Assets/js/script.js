4d7706331a714806b07f17b483f9d6b3e160cd0f
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-01-29 22:33:26 -0500

    added local storage setting. crreated variables for time and text to be set in local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,25 +1,26 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 //add ready for document to load AFTER HTML
 $(document).ready(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
+  //on click needs to set informaition to local storage - time and text so its not deleted
   $('.saveBtn').click(function () {
-    alert('working');
+    // time variable - parent id the parent DIV and attr grabs the id (hour-<number>)
+    // this applies to the click object
+    let time = $(this).parent().attr('id');
+
+    // text variable - sibling element to the button
+    // .description is the class of text area
+    // val method used to return the attribute value
+    let text = $(this).siblings('.description').val();
+
+    // returned values to be SET in local storage
+    localStorage.setItem(text, time);
   });
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
 
   //Ccrate function to get the curretn time in hours
   function getTime() {
@@ -41,16 +42,16 @@ $(document).ready(function () {
         $(this).removeClass('future');
         $(this).removeClass('present');
         $(this).addClass('past');
-      //add class for future  
-      } else if (scheduledTime > presentTime){
-        $(this).removeClass("present");
-        $(this).removeClass("past");
-        $(this).addClass("future");
-      //add class for present  
+        //add class for future
+      } else if (scheduledTime > presentTime) {
+        $(this).removeClass('present');
+        $(this).removeClass('past');
+        $(this).addClass('future');
+        //add class for present
       } else {
-        $(this).removeClass("past");
-        $(this).removeClass("future");
-        $(this).addClass("present");
+        $(this).removeClass('past');
+        $(this).removeClass('future');
+        $(this).addClass('present');
       }
     });
   }
