b4ca6dbc7c397bc19d049c0008fbab0cbbabd321
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-01-31 09:48:34 -0500

    fixed local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -54,19 +54,19 @@ $(document).ready(function () {
   //create GET to select the local storage inputs and place on the page
   // select the id of the time block and then the id of it
   // get the value from the local storage that has been set
-  $('#hour9 .description').val(localStorage.getItem('hour9'));
-  $('#hour10 .description').val(localStorage.getItem('hour10'));
-  $('#hour11 .description').val(localStorage.getItem('hour11'));
-  $('#hour12 .description').val(localStorage.getItem('hour12'));
-  $('#hour13 .description').val(localStorage.getItem('hour13'));
-  $('#hour14 .description').val(localStorage.getItem('hour14'));
-  $('#hour15 .description').val(localStorage.getItem('hour15'));
-  $('#hour16 .description').val(localStorage.getItem('hour16'));
-  $('#hour17 .description').val(localStorage.getItem('hour17'));
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
   // call the function
   getTime();
-  var timer = setInterval(getTime, 15000);
+  // var timer = setInterval(getTime, 15000);
 });
 
 // create variable for date using Moment API
